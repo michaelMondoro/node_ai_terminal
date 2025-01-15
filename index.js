efd3ece8cbb37f52b2dfe531df54e37cf4476737
michael <michaelmondoro@gmail.com>
2025-01-14 23:37:51 -0500

    updated to keep last question for context
    ;

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 import { Client } from "@gradio/client";
 import { performance } from "perf_hooks";
 import readlineSync from "readline-sync";
-import colors from "./colors.js";
-import spinner from "./spinner.js"
+import spinner from "../spinner.js"
+import colors from "../colors.js"
+
+const HUGGING_FACE_MODEL = "mikeymon/mistral2";
+let prior = "";
 
 async function prompt(query) {
   const start = performance.now();
   spinner.start();
 
-  const client = await Client.connect("mikeymon/mistral2");
+  const client = await Client.connect(HUGGING_FACE_MODEL);
   const result = await client.predict("/chat", { 		
-      message: query, 
+      message: prior + "\n" + `User: ${query}`, 
   });
+  prior = `User: ${query}\n Assistant: ${result.data[0]}`;
 
   const total = (performance.now() - start) / 1000;
   spinner.stop();
-  console.log(`[ ${total.toFixed(2)} ${colors.green('sec')} ]\n`)
-  console.log(result.data[0]);
+  console.log(`[ ${colors.cyan(HUGGING_FACE_MODEL)} ] [ ${total.toFixed(2)} sec ]\n`)
+  console.log(`${result.data[0]}\n`);
 
   await client.close();
 }
@@ -25,7 +29,6 @@ async function main() {
   const args = process.argv.slice(2);
   if (args.length > 0) {
     const query = args.join(' ');
-    console.log(`> ${query}`);
     await prompt(query);
     process.exit(0);
   } else {
